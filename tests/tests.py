4b99a413ab894ad20beb942edf23b7b749aeb550
mudolucas <35473986+mudolucas@users.noreply.github.com>
2020-04-09 09:01:56 -0400

    Updated tests.py

--- tests/tests.py
+++ tests/tests.py
@@ -1,48 +1,44 @@
 import os
 import requests
 import unittest
-import psycopg2
-from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, mapper
+import psycopg2
 
-DATABASE_URI = 'postgres+psycopg2://cs162_user:cs162_password@127.0.0.1:5432/cs162'
-
-class Exps(object):
-    pass
-
-engine = create_engine(DATABASE_URI)
-Session = sessionmaker(bind=engine)
-metadata = MetaData(engine)
-expressions = Table('expression', metadata, autoload=True)
-mapper(Exps, expressions)
+DB_URI = 'postgresql://cs162_user:cs162_password@127.0.0.1/cs162?port=5432'
 
 class TestCases(unittest.TestCase):
-    
+
     def setUp(self):
-        s = Session()
-        
+        self.engine = create_engine(DB_URI)
+        self.connection = self.engine.connect()
+        self.conection.execute("DELETE FROM Expression")
+
     def tearDown(self):
-        s.close()
+        self.connection.close()
 
     def test_correct_expression(self):
-        r = requests.post('http://localhost:5000/add', data={'expression': '7+21'})
+        r = requests.post('http://127.0.0.1:5000/add', data={'expression': '7+21'})
         self.assertEqual(r.status_code, 200)
         print(r.text)
         self.assertIn('28',r.text)
 
     def test_expression_db(self):
-        r = requests.post('http://localhost:5000/add', data={'expression': '7+21'})
-        query = s.query(Exps).all()
-        self.assertEqual(len(query),1)
+        r = requests.post('http://127.0.0.1:5000/add', data={'expression': '7+21'})
+        query = self.connection.execute("SELECT COUNT('*') FROM Expression WHERE text='7+21'")
+        rows = query.fetchall()
+
+        self.assertEqual(len(rows),1)
 
     def test_invalid_expression(self):
-        r = requests.post('http://localhost:5000/add', data={'expression': '7+'})
+        r = requests.post('http://127.0.0.1:5000/add', data={'expression': '7+'})
         # Check for internal server error
         self.assertEqual(r.status_code, 500)
 
-        query = s.query(Exps).all()
-        self.assertEqual(len(query), 0)
+        query = self.connection.execute("SELECT COUNT('*') FROM Expression")
+        rows = query.fetchall()
 
+        self.assertEqual(len(rows),0)
 
 if __name__ == '__main__':
     unittest.main()
