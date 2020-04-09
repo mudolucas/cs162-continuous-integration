b48539502b0378372e17f774a598e86566c2b4bf
mudolucas <35473986+mudolucas@users.noreply.github.com>
2020-04-09 08:37:29 -0400

    Updated tests.py
    Server addr for Unix standards

--- tests/tests.py
+++ tests/tests.py
@@ -1,13 +1,11 @@
 import os
 import requests
 import unittest
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, mapper
 import psycopg2
 from sqlalchemy import create_engine, MetaData, Table
 from sqlalchemy.orm import sessionmaker, mapper
 
-DATABASE_URI = 'postgres+psycopg2://cs162_user:cs162_password@192.168.99.100:5432/cs162'
+DATABASE_URI = 'postgres+psycopg2://cs162_user:cs162_password@localhost:5432/cs162'
 
 class Exps(object):
     pass
@@ -25,18 +23,18 @@ class TestCases(unittest.TestCase):
         self.connection.close()
 
     def test_correct_expression(self):
-        r = requests.post('http://192.168.99.100:5000/add', data={'expression': '7+21'})
+        r = requests.post('http://localhost:5000/add', data={'expression': '7+21'})
         self.assertEqual(r.status_code, 200)
         print(r.text)
         self.assertIn('28',r.text)
 
     def test_expression_db(self):
-        r = requests.post('http://192.168.99.100:5000/add', data={'expression': '7+21'})
+        r = requests.post('http://localhost:5000/add', data={'expression': '7+21'})
         query = self.s.query(Exps).all()
         self.assertEqual(len(query),1)
 
     def test_invalid_expression(self):
-        r = requests.post('http://192.168.99.100:5000/add', data={'expression': '7+'})
+        r = requests.post('http://localhost:5000/add', data={'expression': '7+'})
         # Check for internal server error
         self.assertEqual(r.status_code, 500)
 
